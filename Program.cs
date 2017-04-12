4692a631b3489f6ca53f55787bf41dd6bc3e1af6
Thales Henrique Miyashiro Pilao <thales.pilao@fcamara.com.br>
2017-04-12 15:03:56 -0300

    Versionando código v1.0

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncTestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var task1 = TaskWaiter("Task1", 2);
+            var task2 = TaskWaiter("Task2", 6);
+            var task3 = TaskWaiter("Task3", 4);
+
+            Console.Read();
+        }
+
+        private static async Task<string> TaskWaiter(string taskId, int seconds)
+        {
+            await Task.Delay(seconds * 1000);
+
+            Console.WriteLine("Task terminada: " + taskId + " ::: " + DateTime.Now.ToString("MM/dd/yyyy hh:mm:ss.fff"));
+
+            return taskId;
+        }
+    }
+}
